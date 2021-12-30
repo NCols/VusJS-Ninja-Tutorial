fcc346db27f20902085a023952fe264401b9965e
ncols <collignon.n@gmail.com>
2021-12-30 20:17:10 +0100

    Challenge: insert Results comp to display score

---
+++ reaction-timer/src/components/Results.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="result">
+        <p>Your reaction time: {{ score }}ms</p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ["score"],
+}
+</script>
+
+<style>
+    .result {
+        width: 400px;
+        background: rgb(43, 105, 236);
+        margin: 40px auto;
+        padding: 40px 0;
+        text-align: center;
+        color: white;
+        border-radius: 10px;
+    }
+</style>
