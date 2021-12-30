deab9d6b5501ff39dc78b02601fbfaf3e2c8ce27
ncols <collignon.n@gmail.com>
2021-12-30 18:48:45 +0100

    Lifecycle hooks

--- reaction-timer/src/components/Block.vue
+++ reaction-timer/src/components/Block.vue
@@ -1,12 +1,31 @@
 <template>
-  <div class="block">
+  <div class="block" v-if="showBlock">
       Click me
   </div>
 </template>
 
 <script>
 export default {
-    props: ['delay']
+    props: ['delay'],
+    data() {
+        return {
+            showBlock: false
+        }
+    },
+    mounted() { // This is part of the lifecycle hooks. Events related to the lifecycle of the component that fire specific functions like below.
+        console.log('Block component mounted'),
+        console.log('Waiting timeout...'),
+        setTimeout(() => {
+            this.showBlock = true
+            console.log(this.delay)
+        }, this.delay) // Fire callback function avec randomly set delay (passed from App.vue)
+    },
+    updated() {
+        console.log('Component updated')
+    },
+    unmounted() {
+        console.log('Component unmounted')
+    }
 }
 </script>
 
