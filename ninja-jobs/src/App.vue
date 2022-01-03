52bad1ed733cd5c86a773f4665b88f4bf9b5c1e5
ncols <collignon.n@gmail.com>
2022-01-03 18:12:03 +0100

    Programmatic navigation (back, forward & redirect)

--- ninja-jobs/src/App.vue
+++ ninja-jobs/src/App.vue
@@ -5,8 +5,30 @@
     <router-link :to="{ name: 'About' }">About</router-link> |
     <router-link :to="{ name: 'Jobs' }">Jobs</router-link>
   </div>
+
+  <button @click="redirect">Redirect</button>
+  <button @click="back">Go back</button>
+  <button @click="forward">Go forward</button>
+
   <router-view/>
 </template>
+
+<script>
+  export default {
+    methods: {
+      redirect() {
+        this.$router.push({ name: 'Home' })
+      },
+      back() {
+        this.$router.go(-1)   // Do not confuse $route & $router (route to reference our routing, router when we want to do something, like actively route data). Go -1 => go back 1 page
+      },
+      forward() {
+        this.$router.go(1) // Go forward by 1 page
+      },
+    }
+  }
+</script>
+
 
 <style>
 #app {
@@ -32,5 +54,12 @@
 #nav a.router-link-exact-active {
   color: white;
   background: crimson;
+}
+
+button {
+  margin: 0 10px;
+  padding: 10px;
+  border: none;
+  border-radius: 4px;
 }
 </style>
