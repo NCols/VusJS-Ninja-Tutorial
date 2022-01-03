6199593f6f2182c056ad9f161fa32914028d79d8
ncols <collignon.n@gmail.com>
2022-01-03 11:16:09 +0100

    More setup + comments

--- ninja-jobs/src/main.js
+++ ninja-jobs/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router' // We don't need to add '/index.js' here, index is automatically imported.
 
 createApp(App).use(router).mount('#app')
--- ninja-jobs/src/router/index.js
+++ ninja-jobs/src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 
 const routes = [
+  // Whenever we want to create a new route, all we have to do is create the new route in this 'routes' array, then register it in 'const router'.
   {
     path: '/',
     name: 'Home',
@@ -13,12 +15,13 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL), // Allows to use back & forward buttons in browser
   routes
 })
 
