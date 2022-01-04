302da6223df6e36fec787804b77c7e467947ba6e
ncols <collignon.n@gmail.com>
2022-01-04 13:48:40 +0100

    Showcase setup() & composition API

--- nc-blog/src/views/Home.vue
+++ nc-blog/src/views/Home.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="home">
+    <p>Home</p>
   </div>
 </template>
 
 <script>
 export default {
   name: "Home",
-}
+  setup() {
+    // Initialize the Composition API for our component. Similar to the data() function in the ninja-jobs project. This will run before any lifecycle hook (mounted etc).
+    console.log("setup");
+  },
+  created() {
+    console.log("created");
+  },
+  mounted() {
+    console.log("mounted");
+  },
+};
 </script>
