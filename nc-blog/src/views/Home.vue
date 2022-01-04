0f811661ee651b39042e3f2ca9e0d1539cc19291
ncols <collignon.n@gmail.com>
2022-01-04 16:19:48 +0100

    Using refs for reactivity (part 2)

--- nc-blog/src/views/Home.vue
+++ nc-blog/src/views/Home.vue
@@ -3,6 +3,8 @@
     <p>Home</p>
     <p>My name is {{ name }} and my age is {{ age }}.</p>
     <button @click="handleClick">Click me</button>
+    <button @click="age++">Age++</button>
+    <input type="text" v-model="name">
   </div>
 </template>
 
@@ -14,11 +16,12 @@ export default {
   setup() {
     // const p = ref(null)
 
-    const name = ref('mario')
+    const name = ref('Mario')
     const age = ref(30)
 
     const handleClick = () => {
-      name.value = 'luigi' // By default, name and age are not reactive variables. When they change, it won't auto update the values in our template. To make these values in 'setup()' reactive, we can use refs. => const name = ref('mario'). The value of a ref can be accessed with nameOfTheRef.value. When we turn a value into a ref(), we can then pass it as updatable data in the template (witout specifying '.value').
+      name.value = 'Luigi'
+      age.value = 45
     }
 
     return { name, age, handleClick } 
