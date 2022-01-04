e724a458a7684f15678b0948d6e9675ec45ebcaa
ncols <collignon.n@gmail.com>
2022-01-04 12:34:13 +0100

    Conditionnally showing data that we fetch

--- ninja-jobs/src/views/jobs/JobDetails.vue
+++ ninja-jobs/src/views/jobs/JobDetails.vue
@@ -1,26 +1,30 @@
 <template>
-  <h1>{{ job.title }}</h1>
-  <p>The job id is {{ id }}</p>
-  <p>{{ job.details }}</p>
+  <div v-if="job">
+    <h1>{{ job.title }}</h1>
+    <p>Job id: {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+      <p>We're trying to fetch the job details...</p>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['id'], // Receive the 'id' param that is sent via the router-link on Jobs.vue. Allowed because 'props: true' in index.js JobDetails routing.
-    data() {
-        return {
-            job: null,
-        }
-    },
-    mounted() {
-        fetch('http://localhost:3000/jobs/' + this.id) // 'id' here references the prop. In this case we fetch just one object from the db, the one with the id we requested, and we store it in data and assign that to our job property.
-            .then(res => res.json()) 
-            .then(data => this.job = data) 
-            .catch(err => console.log(err.message))
-    }
-}
+  props: ['id'], // Receive the 'id' param that is sent via the router-link on Jobs.vue. Allowed because 'props: true' in index.js JobDetails routing.
+  data() {
+    return {
+      job: null, // 'v-if' in template div avoids getting an error for job = null value, because it waits for job to actually be populated, so that v-if evaluates to true and displays the div with the data.
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id) // 'id' here references the prop. In this case we fetch just one object from the db, the one with the id we requested, and we store it in data and assign that to our job property.
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
 
 <style>
-
 </style>
--- ninja-jobs/src/views/jobs/Jobs.vue
+++ ninja-jobs/src/views/jobs/Jobs.vue
@@ -1,44 +1,49 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-      <router-link :to="{ name: 'JobDetails', params: { id: job.id }}">
-          <h2>{{ job.title }}</h2>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: { id: job.id } }">
+        <h2>{{ job.title }}</h2>
       </router-link>
+    </div>
+  </div>
+  <div v-else>
+      <p>We're trying to load the jobs listing...sorry for the wait</p>
   </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            jobs: [
-               // This time we're going to fetch the 'jobs' data from the json server db and populate our jobs object in here to cycle through it in our template.
-            ]
-        }
-    },
-    mounted() {
-        fetch('http://localhost:3000/jobs')
-            .then(res => res.json()) // When only 1 argument in callback fction, no () needed for 'res' inside 'then'
-            .then(data => this.jobs = data) // We get the data (array object with the jobs data) and assign it to our jobs property.
-            .catch(err => console.log(err.message))
-    },
-}
+  data() {
+    return {
+      jobs: [
+        // This time we're going to fetch the 'jobs' data from the json server db and populate our jobs object in here to cycle through it in our template.
+      ],
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs")
+      .then((res) => res.json()) // When only 1 argument in callback fction, no () needed for 'res' inside 'then'
+      .then((data) => (this.jobs = data)) // We get the data (array object with the jobs data) and assign it to our jobs property.
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
 
 <style>
 .job h2 {
-    background: #f4f4f4;
-    padding: 20px;
-    border-radius: 10px;
-    margin: 10px auto;
-    max-width: 600px;
-    cursor: pointer;
-    color: #444;
+  background: #f4f4f4;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px auto;
+  max-width: 600px;
+  cursor: pointer;
+  color: #444;
 }
 .job h2:hover {
-    background: #ddd;
+  background: #ddd;
 }
 .job a {
-    text-decoration: none;
+  text-decoration: none;
 }
 </style>
