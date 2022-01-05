920b5507c6aeee4396f07a501f045ef57fbf4ca1
ncols <collignon.n@gmail.com>
2022-01-05 13:03:43 +0100

    Computed properties & values

--- nc-blog/src/views/Home.vue
+++ nc-blog/src/views/Home.vue
@@ -1,35 +1,28 @@
 <template>
   <div class="home">
-    <p>Home</p>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update Ninja One</button>
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">Update Ninja Two</button>
+    <h1>Home</h1>
+    <input type="text" v-model="search">
+    <p>Search term: {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">
+      {{ name }}
+    </div>
   </div>
 </template>
 
 <script>
-import { ref, reactive } from 'vue'
+import { computed, ref } from "vue";
 
 export default {
   name: "Home",
   setup() {
-    const ninjaOne = ref({ name: 'Mario', age: 30 })
-    const ninjaTwo = reactive({ name: 'Luigi', age: 35 })
+    const search = ref('')
+    const names = ref(['Mario', 'Yoshi', 'Luigi', 'Toad', 'Bowser', 'Koopa', 'Peach'])
+    
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value)) // Loop through the names array, and check if each name includes the current 'search' value. Now the list of names in the template is updated live depending on whatever letter or term I search for. computed() gives us an updated value based on other values.
+    })
 
-    const updateNinjaOne = () => {
-      ninjaOne.value.age = 40
-    }
-
-    const updateNinjaTwo = () => {
-      ninjaTwo.age = 45 // With reactive, no need to specify '.value' to access & change the value property.
-      // Drawbacks: - We cannot use primitive values in reactive => const x = reactive('luigi') wouldn't work.
-      // We'll rather continue using refs (mostly at least)
-    }
-
-    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo } 
-  }
+    return { names, search, matchingNames };
+  },
 };
 </script>
