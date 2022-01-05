0e7294cd00d1e078c44edf69ecc61a04ac6506ff
ncols <collignon.n@gmail.com>
2022-01-05 13:21:44 +0100

    Stop watch & watchEffect

--- nc-blog/src/views/Home.vue
+++ nc-blog/src/views/Home.vue
@@ -6,6 +6,7 @@
     <div v-for="name in matchingNames" :key="name">
       {{ name }}
     </div>
+    <button @click="handleClick">Stop watching</button>
   </div>
 </template>
 
@@ -18,12 +19,11 @@ export default {
     const search = ref('')
     const names = ref(['Mario', 'Yoshi', 'Luigi', 'Toad', 'Bowser', 'Koopa', 'Peach'])
     
-    watch(search, () => {  // Watches the property 'search' and fires a function every time a change is registered.
+    const stopWatch = watch(search, () => {  // Now if we want to stop watching, we can put the watch() function in a constant, like here, and invoke this new const function at some point. This will effectively stop the watch. Same for watchEffect.
       console.log('Watch function ran')
     })
 
-    watchEffect(() => { // Runs the function every time any dependency in the function changes (any values, variables etc that we put in the function). So here is fires every time search.value changes. So like 'watch()' above, but here we don't have to explicitly specify what we are watching. Just any values that change within the function.
-    // Also, watchEffect runs on page load, so we use watch() if we don't want it to run once every time, or if we do not want/can use a value/variable inside the function.
+    const stopEffect = watchEffect(() => { 
       console.log('WatchEffect function ran', search.value)
     })
 
@@ -31,7 +31,12 @@ export default {
       return names.value.filter((name) => name.includes(search.value))
     })
 
-    return { names, search, matchingNames };
+    const handleClick = () => {
+      stopWatch()
+      stopEffect()
+    }
+
+    return { names, search, matchingNames, handleClick };
   },
 };
 </script>
