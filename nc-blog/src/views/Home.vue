9241e90f691c45b3df7370903a8af6bf131347b6
ncols <collignon.n@gmail.com>
2022-01-05 13:18:33 +0100

    watch & watchEffect

--- nc-blog/src/views/Home.vue
+++ nc-blog/src/views/Home.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-import { computed, ref } from "vue";
+import { computed, ref, watch, watchEffect } from "vue";
 
 export default {
   name: "Home",
@@ -18,8 +18,17 @@ export default {
     const search = ref('')
     const names = ref(['Mario', 'Yoshi', 'Luigi', 'Toad', 'Bowser', 'Koopa', 'Peach'])
     
+    watch(search, () => {  // Watches the property 'search' and fires a function every time a change is registered.
+      console.log('Watch function ran')
+    })
+
+    watchEffect(() => { // Runs the function every time any dependency in the function changes (any values, variables etc that we put in the function). So here is fires every time search.value changes. So like 'watch()' above, but here we don't have to explicitly specify what we are watching. Just any values that change within the function.
+    // Also, watchEffect runs on page load, so we use watch() if we don't want it to run once every time, or if we do not want/can use a value/variable inside the function.
+      console.log('WatchEffect function ran', search.value)
+    })
+
     const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value)) // Loop through the names array, and check if each name includes the current 'search' value. Now the list of names in the template is updated live depending on whatever letter or term I search for. computed() gives us an updated value based on other values.
+      return names.value.filter((name) => name.includes(search.value))
     })
 
     return { names, search, matchingNames };
