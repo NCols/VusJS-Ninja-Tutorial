9f9fe407ceaec161c90d4c2d4c3bfee053f514b7
NCols <collignon.n@gmail.com>
2022-01-11 19:48:36 +0100

    Update config.js

--- live-chat/src/firebase/config.js
+++ live-chat/src/firebase/config.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD1pIY2Ufwk3cMZuNq1l_0euURta-xCWps",
-    authDomain: "net-ninja-tuts-d7028.firebaseapp.com",
-    projectId: "net-ninja-tuts-d7028",
-    storageBucket: "net-ninja-tuts-d7028.appspot.com",
-    messagingSenderId: "1053896890066",
-    appId: "1:1053896890066:web:4005975afda21830c97f0b"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
   // init firebase
