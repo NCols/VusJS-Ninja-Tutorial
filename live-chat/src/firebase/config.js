4a17811ee4ddd4262f78908a541be392e3bbed38
ncols <collignon.n@gmail.com>
2022-01-11 19:48:04 +0100

    Signup and Login views

---
+++ live-chat/src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD1pIY2Ufwk3cMZuNq1l_0euURta-xCWps",
+    authDomain: "net-ninja-tuts-d7028.firebaseapp.com",
+    projectId: "net-ninja-tuts-d7028",
+    storageBucket: "net-ninja-tuts-d7028.appspot.com",
+    messagingSenderId: "1053896890066",
+    appId: "1:1053896890066:web:4005975afda21830c97f0b"
+  };
+
+  // init firebase
+  firebase.initializeApp(firebaseConfig)
+
+  // init firestore service
+  const projectFirestore = firebase.firestore()
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+  export { projectFirestore, timestamp }
